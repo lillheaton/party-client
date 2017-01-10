fca4099fa9211443db15c146712e212b81488f99
Emil Olsson <emiols@lbi.local>
2017-01-10 10:39:50 +0100

    init

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const Koa = require('koa');
+const router = require('koa-router')();
+
+const app = new Koa();
+
+app.use((ctx, next) => {
+  const start = new Date();
+  return next().then(() => {
+    const ms = new Date() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  });
+});
+
+app.use(ctx => {
+	console.log("Jag er 4");
+  ctx.body = 'Hello Koa';
+});
+
+module.exports = app;
