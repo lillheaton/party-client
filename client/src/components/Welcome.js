55ef1223cdedbfe823b6c080bd6b973f9fd3d3c9
Emil Olsson <emilolsson93@gmail.com>
2017-02-25 14:11:51 +0100

    Fix eslint for client

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { graphql, compose  } from 'react-apollo';
 import qql from 'graphql-tag';
 
--- client/src/containers/AppContainer.js
+++ client/src/containers/AppContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
 import Counter from '../components/Counter';
 import Button from '../components/Button';
 import Welcome from '../components/Welcome';
